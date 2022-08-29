45fa016eeda8db333382c0b80ef65eda90f8731e
unhoJly <sokolov_it@mail.ru>
2022-08-30 00:13:43 +0300

    Task 1 is done

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,4 +1,30 @@
 package Task1;
 
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        int[][] resultArray = new int[5][5];
+        long randomNumber, min = 0, max = 0, avg = 0;
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                randomNumber = System.currentTimeMillis() % 10;
+                resultArray[i][j] = (int) randomNumber;
+                avg += randomNumber;
+                System.out.print(resultArray[i][j]);
+
+                if (randomNumber > max)
+                    max = randomNumber;
+                else if (randomNumber < min)
+                    min = randomNumber;
+
+                Thread.sleep(200);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Minimum value is " + min);
+        System.out.println("Average value is " + avg / 25);
+        System.out.println("Maximum value is " + max);
+    }
 }
